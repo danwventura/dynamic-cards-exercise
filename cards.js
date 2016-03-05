e1ea795faaf2e4969776eee1894b18476df157f3
Dan Ventura <danwventura@icloud.com>
2016-03-05 12:39:33 -0600

    adding basic html, necessary tags, variables

---
+++ cards.js
@@ -0,0 +1,8 @@
+//Creating variables
+var create = document.getElementById("create");
+var erase = document.getElementById("delete");
+var input = document.getElementById("input");
+var container = document.getElementById("container");
+var card = document.getElementById("card");
+
+
