28cadba55679f912932c69d5f9b4c4894490513d
Dan Ventura <danwventura@icloud.com>
2016-03-05 12:41:03 -0600

    added blank css file

--- cards.js
+++ cards.js
@@ -3,6 +3,6 @@ var create = document.getElementById("create");
 var erase = document.getElementById("delete");
 var input = document.getElementById("input");
 var container = document.getElementById("container");
-var card = document.getElementById("card");
+var card = document.getElementByClassName("card");
 
 
