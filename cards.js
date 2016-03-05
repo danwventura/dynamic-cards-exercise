4a0d0f99f73bfcb52c3d5e535d5ebbd683f5ec82
Dan Ventura <danwventura@icloud.com>
2016-03-05 15:05:46 -0600

    added function to create card and for loop to select erase buttons and apply event listener for each

--- cards.js
+++ cards.js
@@ -1,8 +1,32 @@
 //Creating variables
 var create = document.getElementById("create");
-var erase = document.getElementById("delete");
+var erase = document.getElementsByClassName("delete");
 var input = document.getElementById("input");
 var container = document.getElementById("container");
-var card = document.getElementByClassName("card");
+var card = document.getElementsByName("card")
+
+// Creating Event Listener to run createCard on click
+create.addEventListener("click", createCard);
+
+//Function to create card HTML
+function createCard(){
+  var output = '<div class="card"> <section> <p class="text">' + input.value + '</p> </section> <input type="button" class="delete" value="Delete"> </div>';
+
+container.innerHTML += output;
+
+DeleteBtnEl();
+}
+
+
+function DeleteBtnEl() {
+  for(var i = 0; i < erase.length; i++){
+    erase[i].addEventListener("click", function(event){
+  card.remove(event.target.parentElement);
+  })
+}
+}
+
+// Creating Event Listener to run deleteCard
+
 
 
